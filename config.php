0ecd05a6dbf4b8771bfaff77f9e0bc2a55960a53
LuisLVar <3005139410101@ingenieria.usac.edu.gt>
2019-07-13 22:04:58 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
-   define('DB_SERVER', 'localhost');
+   define('DB_SERVER', null);
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'administrador');
    define('DB_DATABASE', 'dbmanantialdeoriente');
-   $db = mysqli_connect(DB_SERVER, DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   define('DBPORT', null);
+   define('DBSOCKET','/cloudsql/metal-voyager-246520:us-central1:dbmanantialdeoriente');
+   $db = mysqli_connect(DB_SERVER, DB_USERNAME,DB_PASSWORD,DB_DATABASE, DBPORT, DBSOCKET);
 ?>
