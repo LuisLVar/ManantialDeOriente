9cbc13e3cdffa7eb6dc121967c71f6d9da09f35f
LuisLVar <3005139410101@ingenieria.usac.edu.gt>
2019-07-13 19:31:04 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', '130.211.222.167');
+   define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'administrador');
    define('DB_DATABASE', 'dbmanantialdeoriente');
