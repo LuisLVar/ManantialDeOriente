c542876ecb1bf2c917cea7b96d38a40b0024ef4f
LuisLVar <3005139410101@ingenieria.usac.edu.gt>
2019-07-13 21:49:41 -0600

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'administrador');
    define('DB_DATABASE', 'dbmanantialdeoriente');
-   $db = mysqli_connect(DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $db = mysqli_connect(DB_SERVER, DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
